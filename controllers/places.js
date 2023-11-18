df640dfc250cbf252d61288fa1143cfe22374469
lscolamiero <lautaro.scolamiero@gmail.com>
2023-11-18 13:06:38 -0800

    Adding comments to the code

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,7 @@
+//Creating controller
 const router = require('express').Router()
 
+//Creating first route in places
 router.get('/', function (req, res){
     res.send('GET /places')
 })
--- index.js
+++ index.js
@@ -1,16 +1,27 @@
+//requirung dotenv
 require('dotenv').config()
+
+//requiring express
 const express = require('express')
+
+//Initializing app variable
 const app = express()
 
+//Importing router for places
 app.use('/places', require('./controllers/places'))
 
+//Creating first route
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
+//Creating second route
 app.get('*', function (req, res){
     res.status(404).send('<h1>404 Page</h1>')
 })
 
+//This is just to make sure the server is running
 console.log('I am awake')
+
+//Setting the server to listen on the port set on my .env file
 app.listen(process.env.PORT)
