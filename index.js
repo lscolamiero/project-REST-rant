182e02bec1edbd3ae26be7740cb8477b823ee953
lscolamiero <lautaro.scolamiero@gmail.com>
2023-11-30 19:09:35 -0800

    delete functionality implemented

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 //Modules and Globals
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
 //Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use('/places', require('./controllers/places'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 //ROUTES
 app.get('/', function (req, res) {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,31 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show(data) {
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
+            <main>
+                <div className='showplaces'>
+                    <div className='aligner'>
+                        <img className="img-fluid rounded" src={data.place.pic} alt={data.place.name} height={800} width={800} />
+                    </div>
+                    <div className='aligner2'>
+                        <h1>{data.place.name}</h1>
+                        <h2>Rating</h2>
+                        <h4>Not Rated</h4>
+                        <h2>Description</h2>
+                        <h4>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}.</h4>
+                    </div>
+                </div>
+                <div>
+                    <h1 className='comments border-top'>Comments</h1>
+                    <h4 className='text-center'>No comments yet!!</h4>
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">Delete</button>
+                    </form>
+                </div>
+            </main>
         </Def>
     )
 }
