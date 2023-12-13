002205ad96b35e2b81faeaf4be709735f9afa572
lscolamiero <lautaro.scolamiero@gmail.com>
2023-12-12 19:52:13 -0800

    bonus activity done forms updated

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -8,28 +8,28 @@ function new_form() {
                 <h1>Add a New Place</h1>
                 <form action="/places" method="POST">
                     <div className="row">
-                        <div className="form-group col-sm-6">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label htmlFor="name">Place Name</label>
                             <input className="form-control" id="name" name="name" required />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label for="founded">Founded Year</label>
                             <input className="form-control" id="founded" name="founded" />
                         </div>
 
-                        <div className="form-group col-sm-6">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label htmlFor="pic">Place Picture</label>
                             <input className="form-control" type="url" id="pic" name="pic" />
                         </div>
-                        <div className="form-group col-sm-6">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label htmlFor="city">City</label>
                             <input className="form-control" id="city" name="city" />
                         </div>
-                        <div className="form-group col-sm-6">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label htmlFor="state">State</label>
                             <input className="form-control" id="state" name="state" />
                         </div>
-                        <div className="form-group col-sm-6">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label htmlFor="cuisines">Cuisines</label>
                             <input className="form-control" id="cuisines" name="cuisines" required />
                         </div>
